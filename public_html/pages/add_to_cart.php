58174e5bf3590bc62055118b5c9226ad7b65dcee
Lê Nguyễn Nhựt Linh <lnnlinhcntt2411101@student.ctuet.edu.vn>
2026-07-11 14:01:40 +0700

    Fix lỗi sml

--- public_html/pages/add_to_cart.php
+++ public_html/pages/add_to_cart.php
@@ -4,23 +4,28 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php?error=Bạn cần đăng nhập để sử dụng giỏ hàng");
     exit();
 }
-require_once 'db.php';
-$user_id = $_SESSION['user_id'];
-$book_id = isset($_POST['book_id']) ? intval($_POST['book_id']) : 0;
+try {
 
-if ($book_id > 0) {
-    $stmt = $pdo->prepare("select * from carts where user_id = ? and book_id = ?");
-    $stmt->execute([$user_id, $book_id]);
-    $existing = $stmt->fetch();
+    require_once 'db.php';
+    $user_id = $_SESSION['user_id'];
+    $book_id = isset($_POST['book_id']) ? intval($_POST['book_id']) : 0;
 
-    if ($existing) {
-        $stmt = $pdo->prepare("update carts set quantity = quantity + 1 where cart_id = ?");
-        $stmt->execute([$existing['cart_id']]);
-    } else {
-        $stmt = $pdo->prepare("insert into carts (user_id, book_id, quantity) values (?, ?, 1)");
+    if ($book_id > 0) {
+        $stmt = $pdo->prepare("select * from carts where user_id = ? and book_id = ?");
         $stmt->execute([$user_id, $book_id]);
-    }
-}
+        $existing = $stmt->fetch();
 
-header('Location: ../index.php');
-exit;
+        if ($existing) {
+            $stmt = $pdo->prepare("update carts set quantity = quantity + 1 where cart_id = ?");
+            $stmt->execute([$existing['cart_id']]);
+        } else {
+            $stmt = $pdo->prepare("insert into carts (user_id, book_id, quantity) values (?, ?, 1)");
+            $stmt->execute([$user_id, $book_id]);
+        }
+    }
+
+    header('Location: ../index.php');
+    exit;
+} catch (\Throwable $th) {
+    echo "Lỗi: " . $th->getMessage();
+}
